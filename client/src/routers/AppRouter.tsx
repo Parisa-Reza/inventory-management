1f223a6784905e5cbbcda9f96ba7e3651681c0bd
Parisa Reza <rezaparisa5@gmail.com>
2026-01-07 23:20:38 +0600

    added : react-router layout in AppRouter.tsx

---
+++ client/src/routers/AppRouter.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>root layout</div>,
+    children: [
+      {
+        index: true,
+        element: <div>home page</div>,
+      },
+      {
+        path: "dashboard",
+        element: <div>dashboard page</div>,
+      },
+      {
+        path: "products",
+        element: <div>products page</div>,
+      },
+      {
+        path: "orders",
+        element: <div>orders page</div>,
+      },
+      {
+        path: "reports",
+        element: <div>reports page</div>,
+      },
+      {
+        path: "users",
+        element: <div>users page</div>,
+      },
+      {
+        path: "settings",
+        element: <div>settings page</div>,
+      },
+    ],
+  },
+]);
+
+export const AppRouter = () => {
+  return <RouterProvider router={router} />;
+};
